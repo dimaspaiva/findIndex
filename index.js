ce49763ec323ba40d72da3a1ae9f981b7dec0336
GP-ADM-TI-Dimas Paiva <dimasalpaiva@gmail.com>
2019-03-09 11:04:40 -0300

    Correção

--- index.js
+++ index.js
@@ -9,20 +9,18 @@ const itens = [
 
 // Variável do valor de N a ser encontrado
 const nSearch = 1;
+
+// Variável do valor de M a ser encontrado
 const mSearch = 8;
 
 // Busca pelo index do item 1 passado
-const i1 = itens.findIndex((item, index) =>
-  item.n === nSearch ? index : null
-); // retorna -1
-
-// Busca pelo index do item 2 passado
-const i2 = itens.findIndex((item, index) =>
-  item.m === mSearch ? index : null
-); // retorna 2
+const i1 = itens.findIndex((item, index) => item.n === nSearch); // retorna -1
 
 // Removendo item que teria o valor n = 1
-itens.splice(i1, 1); // Remove o 5º item erroneamente
+itens.splice(i1, 1); // Remove o 1º item corretamente
+
+// Busca pelo index do item 2 passado
+const i2 = itens.findIndex((item, index) => item.m === mSearch); // retorna 2
 
 // Removendo o item que teria valor m = 8
 itens.splice(i2, 1); // Remove o 3º item corretamente
